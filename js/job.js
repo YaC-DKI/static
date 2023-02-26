f1c49e02e699bb26271bd114072f632f3f1f996a
naohr <najmim625@gmail.com>
2023-02-26 13:29:10 +0700

    update: add pre job

---
+++ js/job.js
@@ -0,0 +1,20 @@
+const cScripts = document.getElementsByClassName('c-script');
+
+console.log(cScripts);
+
+window.addEventListener("scroll", () => {
+    if(!navbar.classList.contains("solid-color")){
+        if(window.pageYOffset > document.getElementById("greetings").getBoundingClientRect().bottom && navbar.classList.contains("bs-black") === false){
+            navbar.classList.add("bs-black");
+            Array.from(cScripts).forEach((v) => {
+                console.log(v);
+                v.classList.replace("cs-black", "cs-white");
+            })
+        }else if(window.pageYOffset < document.getElementById("greetings").getBoundingClientRect().bottom && navbar.classList.contains("bs-black")){
+            navbar.classList.remove("bs-black");
+            Array.from(cScripts).forEach((v) => {
+                v.classList.replace("cs-white", "cs-black");
+            })
+        }
+    }
+})
