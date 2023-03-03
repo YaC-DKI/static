5e5e35982eb026e02736e09d1c57354f47462cfa
naohr <najmim625@gmail.com>
2023-03-03 11:39:37 +0700

    update: home

--- js/job.js
+++ js/job.js
@@ -6,11 +6,13 @@ window.addEventListener("scroll", () => {
     if(!navbar.classList.contains("solid-color")){
         if(window.pageYOffset > document.getElementById("greetings").getBoundingClientRect().bottom && navbar.classList.contains("bs-black") === false){
             navbar.classList.add("bs-black");
+            document.getElementById("burger").setAttribute("fill", "white")
             Array.from(cScripts).forEach((v) => {
                 console.log(v);
                 v.classList.replace("cs-black", "cs-white");
             })
         }else if(window.pageYOffset < document.getElementById("greetings").getBoundingClientRect().bottom && navbar.classList.contains("bs-black")){
+            document.getElementById("burger").setAttribute("fill", "black")
             navbar.classList.remove("bs-black");
             Array.from(cScripts).forEach((v) => {
                 v.classList.replace("cs-white", "cs-black");
